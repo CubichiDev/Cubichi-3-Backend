c7b6f6262e89f7f146d12f7b0b8e67e9ea99d6f2
DrTau <drttau@gmail.com>
2024-05-17 01:09:40 +0300

    chore: Add PsqlInteractor to DI container

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Cubichi.DataBase;
 using Cubichi.Services;
 using Microsoft.IdentityModel.Tokens;
 
@@ -13,6 +14,8 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDataBaseInteractor, PsqlInteractor>();
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 
